706fd445720a58b4a195dd7c46689f96315ebea5
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-05-23 14:51:40 +0530

    def self(n):     t=n     c=0     d=0     while n>0:         r=n%10         if r!=0 and t%r==0:             c+=1         n=n//10         d+=1     if d==c:         print(t,end=" ") x=int(input()) y=int(input()) for i in range(x,y+1):     self(i)

---
+++ Self_Dividing_Numbers.py
@@ -0,0 +1,19 @@
+def self(n):
+    t=n
+    c=0
+    d=0
+    while n>0:
+        r=n%10
+        if r!=0 and t%r==0:
+            c+=1
+        n=n//10
+        d+=1
+    if d==c:
+        print(t,end=" ")
+x=int(input())
+y=int(input())
+for i in range(x,y+1):
+    self(i)
+    
+        
+    
