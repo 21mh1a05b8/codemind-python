cdd65592e971e8affc62f6a9c20d4e7b0ecc0135
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-06-29 11:17:54 +0530

    a=input() v="qwertyuioplkjhgfdsazxcvbnm" a=a.lower() r=[] for i in a:     if i in v:         if i not in r:             r.append(i) if len(r)==26:     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,11 +1,12 @@
-n=input()
-n=n.lower()
-a="abcdefghijklmnopqrstuvwxyz"
-f=0
+a=input()
+v="qwertyuioplkjhgfdsazxcvbnm"
+a=a.lower()
+r=[]
 for i in a:
-    if i not in n:
-        f+=1
-if f==0:
+    if i in v:
+        if i not in r:
+            r.append(i)
+if len(r)==26:
     print(True)
 else:
     print(False)
