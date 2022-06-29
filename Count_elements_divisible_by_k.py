f46f4628dabdcf2db8fd10adbf7460556d3e0607
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-06-29 21:47:17 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) c=0 for i in l:     if i%m==0:         c+=1 print(c)

--- Count_elements_divisible_by_k.py
+++ Count_elements_divisible_by_k.py
@@ -1,8 +1,7 @@
-n,k=map(int,input().split())
-a=list(map(int,input().split()))
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
 c=0
-for i in a:
-    if i%k==0:
+for i in l:
+    if i%m==0:
         c+=1
 print(c)
-        
