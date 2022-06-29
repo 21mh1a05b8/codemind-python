f60378c2bd5d1852b7c7785fc8623f76cca66f7a
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-06-29 22:09:27 +0530

    def prime(n):     for i in range(2,n):         if n%i==0:             return False     else:         return True n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if i>1:         if prime(i):             c+=1 print(c)

--- Primes_in_the_array.py
+++ Primes_in_the_array.py
@@ -1,13 +1,13 @@
 def prime(n):
-    for i in range(2,int(n**0.5)+1):
+    for i in range(2,n):
         if n%i==0:
             return False
     else:
         return True
 n=int(input())
-a=list(map(int,input().split()))
+l=list(map(int,input().split()))
 c=0
-for i in a:
+for i in l:
     if i>1:
         if prime(i):
             c+=1
