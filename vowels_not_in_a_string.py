de443ec5320a2e3e8ddef78039fd0fdeecf729a0
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-06-29 11:05:14 +0530

    n=input() n=n.lower() v=list("aeiou") c=0 for i in n:     if i in v:         v.remove(i) if len(v)==0:     print("0") else:     print(*v)

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,11 +1,11 @@
 n=input()
 n=n.lower()
-v=set("aeiou")
+v=list("aeiou")
+c=0
 for i in n:
     if i in v:
         v.remove(i)
 if len(v)==0:
     print("0")
 else:
-    v=sorted(v)
     print(*v)
