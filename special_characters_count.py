2b31077434f50d0ddf9899b7aa40fc5661f1d69a
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-06-29 13:28:43 +0530

    n=input() c=0 for i in n:     if i.isalnum()==False:         if i.isspace()==False:             c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,8 +1,7 @@
 n=input()
 c=0
 for i in n:
-    if i.isdecimal()==False:
-        if i.isalnum()==False:
-            if i.isspace()==False:
-                c+=1
+    if i.isalnum()==False:
+        if i.isspace()==False:
+            c+=1
 print(c)
