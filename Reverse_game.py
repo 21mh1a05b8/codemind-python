6ee33c5f8003715feb5a4644d8f9b971144c13d3
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-07-02 11:26:11 +0530

    def rev(n):     rev=0     while n>0:         r=n%10         rev=(rev*10)+r         n=n//10     return rev n=int(input()) l=list(map(int,input().split())) r=[] for i in l:     r.append(rev(i)) print(*r)

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,11 +1,13 @@
-def palindrome(n):
+def rev(n):
     rev=0
     while n>0:
         r=n%10
         rev=(rev*10)+r
         n=n//10
-    return(rev)
+    return rev
 n=int(input())
-a=list(map(int,input().split()))
-for i in a:
-    print(palindrome(i),end=" ")
+l=list(map(int,input().split()))
+r=[]
+for i in l:
+    r.append(rev(i))
+print(*r)
