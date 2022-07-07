8988d90657480148c61e17c9869e111769fea6dd
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-07-07 19:57:00 +0530

    def sum(n):     sum=0     while(n>0):         r=n%10         sum=sum+r         n=n//10     return sum n=int(input()) l=list(map(int,input().split())) s=0 for i in l:     k=sum(i)     s=s+k print(s)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,14 +1,14 @@
 def sum(n):
     sum=0
-    while n>0:
+    while(n>0):
         r=n%10
         sum=sum+r
         n=n//10
     return sum
 n=int(input())
 l=list(map(int,input().split()))
-su=0
+s=0
 for i in l:
-    s=sum(i)
-    su=su+s
-print(su)
+    k=sum(i)
+    s=s+k
+print(s)
