054740f3eefca32545f8bf666e27c6783371b3c4
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-09-19 14:26:57 +0530

    n,k=map(int,input().split()) l=list(map(int,input().split())) c=0 for i in l:     if i<0:         i=i*-1     i=str(i)     if len(i)==k:         c+=1 print(c)

--- K_digit_length_numbers_in_a_array.py
+++ K_digit_length_numbers_in_a_array.py
@@ -1,25 +1,10 @@
-def count(n):
-    c=0
-    if n<0:
-        n=(n*-1)
-    while n:
-        r=n%10
-        c+=1
-        n=n//10
-    return c
 n,k=map(int,input().split())
 l=list(map(int,input().split()))
-r=[]
 c=0
 for i in l:
     if i<0:
-        l=count(i)
-        r.append(l)
-    else:
-        i=str(i)
-        r.append(len(i))
-c=0
-for i in r:
-    if i==k:
+        i=i*-1
+    i=str(i)
+    if len(i)==k:
         c+=1
 print(c)
