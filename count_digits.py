9fc2ae0207c808d0cc6402f8f7ad728c67cb531c
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-09-19 14:12:54 +0530

    n=int(input()) l=list(map(int,input().split())) for i in l:     if i<0:         i=i*-1     i=str(i)     print(len(i),end=" ")

--- count_digits.py
+++ count_digits.py
@@ -1,17 +1,7 @@
-def count(n):
-    c=0
-    if n<0:
-        n=(n*-1)
-    while n:
-        r=n%10
-        c+=1
-        n=n//10
-    return c
 n=int(input())
 l=list(map(int,input().split()))
 for i in l:
     if i<0:
-        print(count(i),end=" ")
-    else:
-        i=str(i)
-        print(len(i),end=" ")
+        i=i*-1
+    i=str(i)
+    print(len(i),end=" ")
