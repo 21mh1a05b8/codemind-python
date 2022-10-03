e8528280b996501236c846d59aad68fc8c84a6fd
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-03 14:29:07 +0530

    n=input() n=n.split() for i in range(len(n)):     k=n[len(n)-1]     print(k[0])     break

--- last_word_first_character.py
+++ last_word_first_character.py
@@ -1,10 +1,6 @@
 n=input()
 n=n.split()
-r=[]
 for i in range(len(n)):
-    m=n[len(n)-1]
-    for j in m:
-        if j not in r:
-            r.append(j)
-        break
-print(*r)
+    k=n[len(n)-1]
+    print(k[0])
+    break
