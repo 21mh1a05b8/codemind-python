db5e4ae13da1508514b806f8fe0b32a884093237
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-03 14:50:12 +0530

    n=input() n=n.split() for i in n:     print(min(i),end=" ")     print(max(i),end=" ")

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,7 +1,5 @@
 n=input()
 n=n.split()
 for i in n:
-    for j in i:
-        print(min(i),end=" ")
-        print(max(i),end=" ")
-        break
+    print(min(i),end=" ")
+    print(max(i),end=" ")
