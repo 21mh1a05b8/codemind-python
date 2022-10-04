27765165e28bc56b5665a2b08150cebef299de44
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-04 09:13:06 +0530

    n=input() n=n.split() for i in n:     a=ord(min(i))     b=ord(max(i))     print(abs(b-a),end=" ")

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,9 +1,6 @@
 n=input()
 n=n.split()
-m=[]
-mi=[]
-s,s1=0,0
 for i in n:
-    k=ord(max(i))
-    x=ord(min(i))
-    print((abs(x-k)),end=" ")
+    a=ord(min(i))
+    b=ord(max(i))
+    print(abs(b-a),end=" ")
