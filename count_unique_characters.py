008da0f7c41e913a7ed0140d7c1ca86096641027
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-08 12:37:21 +0530

    s=input() s=s.lower() s=list(s) c=0 for i in s:     if(s.count(i)==1 and i!=' '):         c+=1 print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,8 +1,8 @@
-n=input()
-n=n.lower()
-r=[]
-for i in n:
-    if n.count(i)==1:
-        if i!=" ":
-            r.append(i)
-print(len(r))
+s=input()
+s=s.lower()
+s=list(s)
+c=0
+for i in s:
+    if(s.count(i)==1 and i!=' '):
+        c+=1
+print(c)
