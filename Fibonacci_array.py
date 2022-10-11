877e7ecd3055e06421ca1a4982db1725ef642d84
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-11 17:03:16 +0530

    n=int(input()) p=list(map(int,input().split())) c,d=0,0 for i in range(2,n):     if p[i-1]+p[i-2]==p[i]:         c+=1     else:         d+=1 if c>0 and d==0:     print("yes") else:     print("no")

---
+++ Fibonacci_array.py
@@ -0,0 +1,12 @@
+n=int(input())
+p=list(map(int,input().split()))
+c,d=0,0
+for i in range(2,n):
+    if p[i-1]+p[i-2]==p[i]:
+        c+=1
+    else:
+        d+=1
+if c>0 and d==0:
+    print("yes")
+else:
+    print("no")
