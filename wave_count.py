b61240740b2dd1a45f924698da802df4b87623ab
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-11 17:15:08 +0530

    n=int(input()) k=list(map(int,input().split())) c,g=0,0 for i in range(1,n-1,2):     if k[i-1]<k[i]:         c+=1     else:         g+=1 if c>0 and g==0:     print(c) else:     print("-1")

---
+++ wave_count.py
@@ -0,0 +1,12 @@
+n=int(input())
+k=list(map(int,input().split()))
+c,g=0,0
+for i in range(1,n-1,2):
+    if k[i-1]<k[i]:
+        c+=1
+    else:
+        g+=1
+if c>0 and g==0:
+    print(c)
+else:
+    print("-1")
