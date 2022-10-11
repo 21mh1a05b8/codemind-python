cbc2f4ce5b6ccdf9363bc7a8fa1a6ead64e61bcd
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-11 16:40:44 +0530

    n=int(input()) k=list(map(int,input().split())) c=[] l=[] for i in range(n):     if k[i]%2==0:         c.append(k[i])     else:         l.append(k[i]) i,j=0,0 while i<len(c) or j<len(l):     if i<len(c):         print(c[i],end=" ")         i+=1     if j<len(l):         print(l[j],end=" ")         j+=1 if n%2!=0:     print("0")

---
+++ even_odd_pairs.py
@@ -0,0 +1,20 @@
+n=int(input())
+k=list(map(int,input().split()))
+c=[]
+l=[]
+for i in range(n):
+    if k[i]%2==0:
+        c.append(k[i])
+    else:
+        l.append(k[i])
+i,j=0,0
+while i<len(c) or j<len(l):
+    if i<len(c):
+        print(c[i],end=" ")
+        i+=1
+    if j<len(l):
+        print(l[j],end=" ")
+        j+=1
+if n%2!=0:
+    print("0")
+        
